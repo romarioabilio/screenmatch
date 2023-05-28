704bdfff14fd17beae1fe6066aee008c678371f6
Romário Abílio <romarioabilio22082001@gmail.com>
2023-05-28 17:14:55 -0300

    Adicionando atributos a classe filme

---
+++ screenmatch/src/Models/Filme.java
@@ -0,0 +1,11 @@
+package Models;
+public class Filme{
+    private String nome;
+    private int anoDeLancamento;
+    private boolean incluidoNoPlano;
+    private double avaliacao;
+    private int totalDeAvaliacoes;
+    private int duracaoEmMinutos;
+}
+
+
