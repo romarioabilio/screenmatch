5cab7f9c378bf7fbbd32120dde33aabf927047cb
Romário Abílio <romarioabilio22082001@gmail.com>
2023-05-28 23:33:51 -0300

    Praticando encapsulamento

--- screenmatch/src/Models/Filme.java
+++ screenmatch/src/Models/Filme.java
@@ -1,29 +1,91 @@
 package Models;
+
 public class Filme {
-    String nome;
-    int anoDeLancamento;
-    boolean incluidoNoPlano;
-    double somaDasAvaliacoes;
-    int totalDeAvaliacoes;
-    int duracaoEmMinutos;
+    private String nome;
+    private int anoDeLancamento;
+    private boolean incluidoNoPlano;
+    private double somaDasAvaliacoes;
+    private int totalDeAvaliacoes;
+    private int duracaoEmMinutos;
+
 
 
 void exibiFichaTecnica(){
-    System.out.println("Nome do filme: " + this.nome);
-    System.out.println("Ano de lançamento: " + this.anoDeLancamento);
-    System.out.println("Avaliação: " +this.somaDasAvaliacoes);
-    System.out.println("Incluido no plano ? "+ this.incluidoNoPlano);
-    System.out.println("Total de avaliações: " + this.totalDeAvaliacoes);
-    System.out.println("Duração do filme: " + this.duracaoEmMinutos+" minutos");
+    System.out.println("Nome do filme: " + getNome());
+    System.out.println("Ano de lançamento: " +getAnoDeLancamento());
+    System.out.println("Soma de Avaliações: " +getSomaDasAvaliacoes());
+    System.out.println("Incluido no plano ? "+getIncluidoNoPlano());
+    System.out.println("Total de avaliações: " +getTotalDeAvaliacoes());
+    System.out.println("Duração do filme: " +getDuracaoEmMinutos()+" minutos");
+    System.out.println("Média de avaliações: "+getMediaDasAvaliacoes());
 }
 
+
+//PEGA O NOME
+String getNome(){
+    return nome;
+}
+//MODIFICA O NOME
+void setNome(String nome){
+    this.nome = nome ;
+}
+
+
+//PEGA O ANO DE LANÇAMENTO
+int getAnoDeLancamento(){
+    return anoDeLancamento;
+}
+//MODIFICA O ANO DE LANÇAMENTO
+void setAnoDeLancamento(int anoDeLancamento){
+    this.anoDeLancamento = anoDeLancamento;
+}
+
+
+//PEGA A SOMA DE TODAS AVALIAÇÕES
+double getSomaDasAvaliacoes(){
+    return somaDasAvaliacoes;
+}
+
+
+
+//PEGA SE O FILME ESTÁ INCLUIDO NO PLANO MENSAL
+boolean getIncluidoNoPlano(){
+    return incluidoNoPlano;
+}
+//MODIFICA A INCLUSÃO NO PLANO MENSAL
+void setIncluidoNoPlano(boolean incluidoNoPlano){
+    this.incluidoNoPlano = incluidoNoPlano;
+}
+
+
+//PEGA O TOTAL DE AVALIAÇOES
+int getTotalDeAvaliacoes(){
+    return totalDeAvaliacoes;
+}
+
+
+
+//PEGA A DURAÇÃO DO FILME
+int getDuracaoEmMinutos(){
+    return duracaoEmMinutos;
+}
+//MODIFICA A DURAÇÃO DO FILME
+void setDuracaoEmMinutos(int duracaoEmMinutos){
+    this.duracaoEmMinutos = duracaoEmMinutos;
+}
+
+
+//DA UMA NOTA PRA O FILME
 void avalia(double nota){
     this.somaDasAvaliacoes += nota;
     this.totalDeAvaliacoes ++;
-    System.out.println(somaDasAvaliacoes);
-    System.out.println(totalDeAvaliacoes);
 } 
 
+//PEGA A MEDIA DE TODAS AVALIAÇÕES
+double getMediaDasAvaliacoes(){
+    return this.somaDasAvaliacoes / this.totalDeAvaliacoes;
+
+}
 
 
 
--- screenmatch/src/Models/Principal.java
+++ screenmatch/src/Models/Principal.java
@@ -3,16 +3,22 @@ package Models;
 public class Principal {
     public static void main(String[] args) {
         Filme filme = new Filme();
-        filme.nome = "Madagascar";
-        filme.duracaoEmMinutos = 165;
-        filme.anoDeLancamento = 2005;
-       // filme.incluidoNoPlano = true;
-        //filme.totalDeAvaliacoes = 1334;
-       // filme.somaDasAvaliacoes = 4.9;
+        //filme.nome = "Madagascar";
+        filme.setNome("Madagascar");
+        //filme.duracaoEmMinutos = 165;
+        filme.setDuracaoEmMinutos(165);
+        //filme.anoDeLancamento = 2005;
+        filme.setAnoDeLancamento(2008);
+        //filme.incluidoNoPlano = true;
+        filme.setIncluidoNoPlano(true);
+        filme.avalia(9);
+        filme.avalia(8);
+        filme.avalia(7);
         filme.exibiFichaTecnica();
-        filme.avalia(1.0);
-        filme.avalia(8.9);
-        filme.avalia(9.5);
+        
+       
+      
+     
 
     }
 }
